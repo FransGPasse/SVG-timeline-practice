a1eab94ec267318ff5f37dbec123ba24dd874fe7
Frans Gustavson Påsse <frans.gop@hotmail.com>
2022-01-26 13:21:59 +0100

    Removed childish nonsense, changed background, going to create a more advanced SVG soon

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 let path = document.querySelector("path");
 let pathLength = path.getTotalLength();
 
-path.style.strokeDasharray = pathLength + " " + pathLength; 
+path.style.strokeDasharray = pathLength + " " + pathLength;
 
 path.style.strokeDashoffset = pathLength;
 
 window.addEventListener("scroll", () => {
+  //What % down is it?
+  var scrollPercentage =
+    (document.documentElement.scrollTop + document.body.scrollTop) /
+    (document.documentElement.scrollHeight -
+      document.documentElement.clientHeight);
 
-    //What % down is it?
-    var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+  // Length to offset the dashes
+  var drawLength = pathLength * scrollPercentage;
 
-    // Length to offset the dashes
-    var drawLength = pathLength * scrollPercentage;
+  //Draw in reverse
+  path.style.strokeDashoffset = pathLength - drawLength;
 
-    //Draw in reverse
-    path.style.strokeDashoffset = pathLength - drawLength;
+
+  //Potential parallax effects but didn't work
+  const target = document.querySelectorAll(".scroll");
+
+  var index = 0,
+    lenght = target.length;
+  for (index; index < length; index++) {
+    var pos = window.pageYOffset * target[index].dataset.rate;
+
+    if (target[index].dataset.direction === "vertical") {
+      target[index].style.transform = "translate3d(0px," + pos + "px, 0px)";
+    } else {
+      var posX = window.pageYOffset * target[index].dataset.ratex;
+      var posY = window.pageYOffset * target[index].dataset.ratey;
+
+      target[index].style.transform =
+        "translate3d(" + posX + "px, " + posY + "px, 0px)";
+    }
+  }
 });
